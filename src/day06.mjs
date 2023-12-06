9bf253a58acb716760fc6be60b7d081644f2afb2
Thomas Domingues <thomasdomingues@icloud.com>
2023-12-06 22:55:05 +0100

    day 06 bonus

--- src/day06.mjs
+++ src/day06.mjs
@@ -24,3 +24,35 @@ async function main() {
 }
 
 main().catch(console.error);
+
+async function bonus() {
+	const input = await readFile(resolve("input/day06.txt"), {
+		encoding: "utf8",
+	});
+
+	const [time, distance] = input
+		.split("\n")
+		.map((line) =>
+			line.match(/\d+/g).reduce((prev, curr) => `${prev}${curr}`, ""),
+		)
+		.map(Number);
+
+	let nbRecordWinMethods = 0;
+	let hasReachedFirstWinMethods = false;
+	for (let speed = 0; speed < distance; ++speed) {
+		const dist = (time - speed) * speed;
+		if (dist <= distance && !hasReachedFirstWinMethods) {
+			continue;
+		}
+		if (dist > distance) {
+			hasReachedFirstWinMethods = true;
+			nbRecordWinMethods += 1;
+			continue;
+		}
+		break;
+	}
+
+	console.log("Bonus:", nbRecordWinMethods);
+}
+
+bonus().catch(console.error);
