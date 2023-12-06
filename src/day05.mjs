b54a4a90f2faaaaf632baf2edebfd2baaaed8988
Thomas Domingues <thomasdomingues@icloud.com>
2023-12-06 01:53:22 +0100

    day 05 bonus

--- src/day05.mjs
+++ src/day05.mjs
@@ -1,12 +1,25 @@
 import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
+import sortBy from "lodash/sortBy.js";
 
 function get(map, key) {
+	if (
+		key < map[0][1] ||
+		key >= map[map.length - 1][1] + map[map.length - 1][2]
+	) {
+		return key;
+	}
+
 	for (const [dst, src, inc] of map) {
+		if (src > key) {
+			return key;
+		}
+
 		if (key >= src && key < src + inc) {
 			return key + (dst - src);
 		}
 	}
+
 	return key;
 }
 
@@ -46,7 +59,7 @@ function parseMap(input) {
 		[...rawMap.matchAll(/\d+/g)].map((n) => parseInt(n, 10)),
 	);
 
-	return parsedMaps;
+	return sortBy(parsedMaps, 1);
 }
 
 function parseData(input) {
@@ -72,9 +85,58 @@ async function main() {
 	});
 
 	const { seeds, ...maps } = parseData(input);
+	console.log(maps);
 	const locations = seeds.map((seed) => mapSeedToLocation(seed, maps));
 
 	console.log("Main:", Math.min(...locations));
 }
 
 main().catch(console.error);
+
+async function bonus() {
+	const input = await readFile(resolve("input/day05.txt"), {
+		encoding: "utf8",
+	});
+
+	const [seedsLine, ...mappingLines] = input.split("\n\n");
+	const seeds = seedsLine.split(":")[1].trim().split(" ").map(Number);
+	const mapMatrix = mappingLines.map((line) =>
+		line
+			.split("\n")
+			.slice(1)
+			.map((s) => s.split(" ").map(Number))
+			.map(([dStart, sStart, length]) => ({
+				dStart,
+				dEnd: dStart + length - 1,
+				sStart,
+				sEnd: sStart + length - 1,
+			})),
+	);
+
+	const candidateSeeds = mapMatrix
+		.flatMap((mappings, i) =>
+			mappings.map((m) =>
+				mapMatrix.slice(0, i + 1).reduceRight((curr, mm) => {
+					const n = mm.find((n) => curr >= n.dStart && curr <= n.dEnd);
+					return n ? n.sStart + (curr - n.dStart) : curr;
+				}, m.dStart),
+			),
+		)
+		.filter((seed) =>
+			seeds.some((s, i) => i % 2 === 0 && seed >= s && seed < s + seeds[i + 1]),
+		);
+
+	console.log(
+		"Bonus:",
+		Math.min(
+			...candidateSeeds.map((val) =>
+				mapMatrix.reduce((curr, mappings) => {
+					const m = mappings.find((m) => curr >= m.sStart && curr <= m.sEnd);
+					return m ? m.dStart + (curr - m.sStart) : curr;
+				}, val),
+			),
+		),
+	);
+}
+
+bonus().catch(console.error);
