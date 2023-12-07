fa85c8e53619a085ff3a1794545308e22b0747de
Thomas Domingues <thomas.domingues.ext@francetv.fr>
2023-12-07 18:20:45 +0100

    day 07 main

--- src/day07.mjs
+++ src/day07.mjs
@@ -1,87 +1,40 @@
-import countBy from "lodash/countBy.js";
-import sortBy from "lodash/sortBy.js";
 import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
-import { inspect } from "node:util";
-
-const CARDS = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 
 const CARD_POINTS = {
-	A: 14,
-	K: 13,
-	Q: 12,
-	J: 11,
-	T: 10,
-	9: 9,
-	8: 8,
-	7: 7,
-	6: 6,
-	5: 5,
-	4: 4,
-	3: 3,
-	2: 2,
+	A: 12,
+	K: 11,
+	Q: 10,
+	J: 9,
+	T: 8,
+	9: 7,
+	8: 6,
+	7: 5,
+	6: 4,
+	5: 3,
+	4: 2,
+	3: 1,
+	2: 0,
 };
 
 const HAND_POINTS = {
-	5: 7,
-	4: 6,
-	32: 5,
-	3: 4,
-	22: 3,
-	2: 2,
-	1: 1,
+	fiveOfAKind: 7,
+	fourOfAKind: 6,
+	fullHouse: 5,
+	threeOfAKind: 4,
+	twoPair: 3,
+	onePair: 2,
+	highCard: 1,
 };
 
 function parseGames(input) {
-	return input
-		.split("\n")
-		.map((line) => [line.split(" ")[0], parseInt(line.split(" ")[1], 10)])
-		.map(([hand, bid]) => [
-			hand
-				.split("")
-				.sort((cardA, cardB) => CARD_POINTS[cardB] - CARD_POINTS[cardA])
-				.join(""),
-			bid,
-		]);
-}
+	return input.split("\n").map((line) => {
+		const [rawCards, rawBid] = line.split(" ");
+		const cards = rawCards.split("");
+		const bid = parseInt(rawBid);
 
-function rankGames(games) {
-	return sortBy(
-		games
-			.map(([hand, bid]) => [countBy(hand), bid])
-			.map(([hand, bid]) => [
-				sortBy(
-					Object.entries(hand).filter(
-						([, value]) =>
-							value ===
-							Math.max(...Object.entries(hand).map(([, value]) => value)),
-					),
-					[([, nbCards]) => -nbCards, ([card]) => -CARD_POINTS[card]],
-				),
-				bid,
-			])
-			.map(([hand, bid]) => [
-				hand.reduce(
-					(prev, curr, i) => ({
-						hand: `${prev.hand}${curr[1]}`,
-						firstCardPoints:
-							i === 0 ? CARD_POINTS[curr[0]] : prev.firstCardPoints,
-						secondCardPoints: i === 1 ? CARD_POINTS[curr[0]] : 0,
-					}),
-					{
-						hand: "",
-						firstCardPoints: 0,
-						secondCardPoints: 0,
-					},
-				),
-				bid,
-			]),
-		[
-			([{ hand }]) => HAND_POINTS[hand],
-			([{ firstCardPoints }]) => firstCardPoints,
-			([{ secondCardPoints }]) => secondCardPoints,
-		],
-	).map(([, bid], i) => [i + 1, bid]);
+		return { cards, bid };
+	});
 }
 
 async function main() {
@@ -89,16 +42,47 @@ async function main() {
 		encoding: "utf8",
 	});
 
-	const games = parseGames(input);
+	const rawHands = parseGames(input);
+	const hands = rawHands.map((hand) => {
+		const cardPoints = new Array(13).fill(0);
+		for (const card of hand.cards) {
+			cardPoints[CARD_POINTS[card]] += 1;
+		}
+		const orderedStrengths = cardPoints.sort((a, b) => b - a);
+		let type = HAND_POINTS.highCard;
+		if (orderedStrengths[0] === 5) {
+			type = HAND_POINTS.fiveOfAKind;
+		} else if (orderedStrengths[0] === 4) {
+			type = HAND_POINTS.fourOfAKind;
+		} else if (orderedStrengths[0] === 3 && orderedStrengths[1] === 2) {
+			type = HAND_POINTS.fullHouse;
+		} else if (orderedStrengths[0] === 3) {
+			type = HAND_POINTS.threeOfAKind;
+		} else if (orderedStrengths[0] === 2 && orderedStrengths[1] === 2) {
+			type = HAND_POINTS.twoPair;
+		} else if (orderedStrengths[0] === 2) {
+			type = HAND_POINTS.onePair;
+		}
+		return { ...hand, cardPoints, type };
+	});
 
-	const rankedGames = rankGames(games);
+	const orderedHands = hands.sort((a, b) => {
+		if (a.type !== b.type) {
+			return b.type - a.type;
+		}
+		for (let i = 0; i < a.cards.length; i++) {
+			if (a.cards[i] !== b.cards[i]) {
+				return CARD_POINTS[b.cards[i]] - CARD_POINTS[a.cards[i]];
+			}
+		}
+		return 0;
+	});
 
-	console.log(inspect(rankedGames, undefined, Number.POSITIVE_INFINITY, true));
+	const solution = orderedHands.reduce((acc, hand, index) => {
+		return acc + hand.bid * (orderedHands.length - index);
+	}, 0);
 
-	console.log(
-		"Main:",
-		rankedGames.reduce((count, hand) => count + hand[0] * hand[1], 0),
-	);
+	console.log("Main:", solution);
 }
 
 main().catch(console.error);
